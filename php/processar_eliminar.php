afec0d327c7aff239f0b7ba3b8751ffdebad8d33
Afonso_Marques <afonsolobo945@gmail.com>
2025-01-10 08:47:50 +0000

    processar_eliminar

--- php/processar_eliminar.php
+++ php/processar_eliminar.php
@@ -55,38 +55,6 @@ if (isset($_REQUEST['apagar'])) {
         </tr>
     </table>
 </div>
-
-
-
-
-
-
-
-
-<div>
-<table class="tabela">
-<tr><font face="Arial" align="center">Dados do utilizador a eliminar:</tr><br />
-
-<?php
-$id = $_GET['id'];
-$nome_utilizador = $_GET['nome_utilizador'];
-$email = $_GET['email'];
-?>
-
-<tr><td width="100px" align="left" bgcolor="99cc33"><font face="Arial" size=2>Nº registo: <?php echo htmlspecialchars($id); ?></td></tr>
-<tr><td width="200px" align="left" bgcolor="99cc33"><font face="Arial" size=2>Nome de utilizador: <?php echo htmlspecialchars($nome_utilizador); ?></td></tr>
-<tr><td width="300px" align="left" bgcolor="99cc33"><font face="Arial" size="2">Endereço de correio eletrónico: <?php echo htmlspecialchars($email); ?></td></tr>
-<tr></tr>
-<tr><td>
-<form method="POST" action="processar_eliminar.php">
-    Pretende mesmo eliminar este registo?
-    <input type="submit" name="apagar" value="Sim">
-    <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
-</form>
-</td>
-</tr>
-</table>
-</div>
 <?php 
 }
 ?>
